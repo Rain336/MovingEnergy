72056250cecc8f7f5785cdf3e08e7c4483f0fe08
Rain336 <rain336@live.de>
2017-07-29 16:04:55 +0200

    updated Debug Map!

--- assets/debug/debug.tsx
+++ assets/debug/debug.tsx
@@ -1,4 +1,14 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="debug" tilewidth="32" tileheight="32" tilecount="4" columns="2">
  <image source="debug.png" width="64" height="64"/>
+ <tile id="0">
+  <properties>
+   <property name="friction" type="float" value="200"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="restitution" type="float" value="200"/>
+  </properties>
+ </tile>
 </tileset>
