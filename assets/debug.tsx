a055d7fa34e56134815f1f60e950a9fdafc08f26
Rain336 <rain336@live.de>
2017-07-29 10:57:12 +0200

    Added Debug Level

---
+++ assets/debug.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="debug" tilewidth="32" tileheight="32" tilecount="4" columns="2">
+ <image source="debug.png" width="64" height="64"/>
+</tileset>
