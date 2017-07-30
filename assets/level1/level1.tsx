c7d951c92ec45f44bc7a23c8fc44261672954d29
Rain336 <rain336@live.de>
2017-07-30 14:08:32 +0200

    Added First Level!

---
+++ assets/level1/level1.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Level1" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+ <image source="level1.png" width="128" height="128"/>
+ <tile id="12">
+  <properties>
+   <property name="trigger" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="trigger" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="trigger" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="trigger" type="bool" value="false"/>
+  </properties>
+ </tile>
+</tileset>
